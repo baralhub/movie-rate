dfaea45de8cd98062bcf8761ae9305410a50724b
Pradeep <mailtobaral@gmail.com>
2019-09-24 17:45:34 -0700

    Added Boilerplate Pages and Routes

--- client/src/components/elements/MovieIcons/MovieIcons.js
+++ client/src/components/elements/MovieIcons/MovieIcons.js
@@ -1,10 +1,17 @@
 import React from 'react';
 import './MovieIcons.css';
+import { Link } from 'react-router-dom';
 
 const MovieIcons = (props) => {
     return (
         <div className="rmdb-moviethumb">
+            {props.clickable ?
+            <Link to={{ pathname: `/${props.movieId}`, movieName: `${props.movieName}` }}>
             <img src={props.image} alt="moviethumb" />
+            </Link>
+            :
+            <img src={props.image} alt="moviethumb" />
+            }
         </div>
     )
 }
